531f397fa0bde20173de4e6192595b2835f441c1
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2021-10-06 17:53:43 +0100

    input

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 // 5 judges judges code score 1-10, some too low/high, 2,9
 // 1,2,7,9,9
 
-function standlize(array) {
+function standlize(array, lowThreshold, highThreshold) {
   // find the outscrope number and round it
-
   const newArray = array.map((score) => {
-    if (score > 9) {
-      score = 9;
-    }
-    if (score < 2) {
-      score = 2;
-    }
-    return score;
+    if (score > highThreshold) return highThreshold;
+    else if (score < lowThreshold) return lowThreshold;
+    else return score;
   });
-
+  console.log(newArray);
   return newArray;
 }
+
+standlize([2, 3, 4, 5, 6], 4, 7);
+
+class Stand {
+  standlize(array, lowThreshold, highThreshold) {
+    const newArray = array.map((score) => {
+      if (score > highThreshold) return highThreshold;
+      else if (score < lowThreshold) return lowThreshold;
+      else return score;
+    });
+    console.log(newArray);
+    return newArray;
+  }
+}
+
+const standInstance = new Stand();
+
+standInstance.standlize([2, 3, 4, 5, 6], 4, 7);
